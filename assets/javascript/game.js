00b37e093d5df94cf3492986cbfeede8090f055a
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-10-17 21:55:50 -0700

    Final additions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,41 +2,51 @@ $(document).ready(function(){
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
+var scoreDisplay = 0;
 
 //Seclects a random number between 19 - 120 
-var randomNum = Math.floor(Math.random() * 101 + 19)
+var randomNum = Math.floor(Math.random() * 101 + 19);
+
+$("#randomNum").text(randomNum);
 
 //Randoms numbers for each crystal between 1 - 12
 
-var image1 = Math.floor(Math.random() * 11 + 1)
-var image2 = Math.floor(Math.random() * 11 + 1)
-var image3 = Math.floor(Math.random() * 11 + 1)
-var image4 = Math.floor(Math.random() * 11 + 1)
+var image1 = Math.floor(Math.random() * 11 + 1);
+var image2 = Math.floor(Math.random() * 11 + 1);
+var image3 = Math.floor(Math.random() * 11 + 1);
+var image4 = Math.floor(Math.random() * 11 + 1);
 
-$("#randomNum").text(randomNum);
+
 
 
 $("#wins").text(wins);
 $("#losses").text(losses);
+$("#scoreDisplay").text(scoreDisplay);
 
 
 function won(){
     wins++;
     alert("You won!");
     $("#wins").text(wins);
+    restart();
 }
 
 
 function lost(){
     losses++;
-    alert("Sorry, you lose! Better luck next time..")
+    alert("Sorry, you lose! Better luck next time..");
     $("#losses").text(losses);
+    restart();
+    
 }
+
+//Set up clicks for crystals
 
 $("#cry1").on("click",function(){
     totalScore = totalScore + image1;
     $("#scoreDisplay").text(totalScore);
     console.log(totalScore);
+//win/lose conditionals
     if (totalScore === randomNum){
         won();
     }
@@ -44,7 +54,7 @@ $("#cry1").on("click",function(){
         lost();
     }
 
-})
+    })
 
 $("#cry2").on("click", function(){
 
@@ -58,7 +68,7 @@ $("#cry2").on("click", function(){
         lost();
     }
 
-})
+    })
 
 $("#cry3").on("click", function(){
     totalScore = totalScore + image3;
@@ -71,7 +81,7 @@ $("#cry3").on("click", function(){
         lost();
     }
 
-})
+    })
 
 $("#cry4").on("click", function(){
     totalScore = totalScore + image4;
@@ -84,7 +94,20 @@ $("#cry4").on("click", function(){
         lost();
     }
 
-});
+    });
+
+function restart(){
+    randomNum = Math.floor(Math.random() * 101 + 19)
+    $("#randomNum").text(randomNum);
+    image1 = Math.floor(Math.random() * 11 + 1);
+    image2 = Math.floor(Math.random() * 11 + 1);
+    image3 = Math.floor(Math.random() * 11 + 1);
+    image4 = Math.floor(Math.random() * 11 + 1);  
+    scoreDisplay = 0;
+    $("#scoreDisplay").text(scoreDisplay);
+    totalScore = 0;
+    }
 
 
-});
+
+}); 
